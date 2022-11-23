13ab4073c3e70dbdf118179b8a9a19ae4509c54f
CrystianPrintes <63503019+CrystianPrintes20@users.noreply.github.com>
2022-11-23 19:26:55 -0300

    Add: Carousel

--- src/pages/home_initial/index.js
+++ src/pages/home_initial/index.js
@@ -3,13 +3,16 @@ import SideNavBar from "componentes/SiderBar";
 import posts from "json/posts.json";
 import PostCard from "componentes/PostCard";
 import Container from '@mui/material/Container';
+import CarouselComponent from "componentes/Carousel/Carousel_Component";
 
 
 export default function Home_initial() {
   return (
     <>
       <SideNavBar />
+      <CarouselComponent/>
       <Container maxWidth="sm">
+        
     </Container>
     </>
   );
