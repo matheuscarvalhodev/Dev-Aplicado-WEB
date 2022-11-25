c3d4f938843b1bdac0ebece9e88feadf89da265f
CrystianSkalena <crystian.printes@skalena.com>
2022-11-25 12:30:02 -0300

    add:ImagesCarousel

--- src/componentes/Carousel/Carousel_Component.js
+++ src/componentes/Carousel/Carousel_Component.js
@@ -9,19 +9,19 @@ import {
 
 const items = [
   {
-    src: 'https://picsum.photos/id/123/800/400',
+    src: './DefesaCivil.png',
     altText: 'Slide 1',
     caption: 'Slide 1',
     key: 1,
   },
   {
-    src: 'https://picsum.photos/id/456/800/400',
+    src: './Defesacivil2.jpg',
     altText: 'Slide 2',
     caption: 'Slide 2',
     key: 2,
   },
   {
-    src: 'https://picsum.photos/id/678/800/400',
+    src: './Defesacivil3.jpeg',
     altText: 'Slide 3',
     caption: 'Slide 3',
     key: 3,
