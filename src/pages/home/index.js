2ae353b5c7ae63d81cc07e45ef9389999adfa82d
Marcos Printes <67763353+MacosPrintes001@users.noreply.github.com>
2022-11-30 09:37:55 -0300

    tela home v0

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,38 +1,54 @@
-import { Card, Button, Col, Row } from "reactstrap";
+import { Card, Button, Col, Row, CardHeader, CardBody } from "reactstrap";
 import styles from './Inicio.module.css';
 
 export default function Home() {
     return (
-        <div >
+        <div>
+            <div className="container" style={{ marginTop: "100px" }}>
+                <CardHeader>
+                    <strong>
+                        <h4 className="text-center"
+                            style={{ fontSize: 60, fontFamily:'initial' }}>
 
-            <Row className={styles.card}>
-                <Col className={styles.semma}>
-                    <div className="m-3">
-                        <h1>ESCOLHA A PLATAFORMA </h1>
-                    </div>
-                    <div>
-                        <img src="SEMMA_LOGO.png" className={styles.imagem} />
-                    </div>
-                    <Button className={styles.buttonSemma}>
-                        SEMMA
-                    </Button>
-                </Col>
+                            SEJA BEM-VINDO(A)!
+                        </h4>
+                    </strong>
 
-                <Col className={styles.meioAmbiente}>
-                    <div className="m-3">
-                        <h1>QUE DESEJA FAZER LOGIN</h1>
-                    </div>
-                    <div>
-                        <img src="DFCIVIL.png" className={styles.imagem} />
-                    </div>
-                    <div d-flex flex-column align-items-center>
-                        <Button onClick='href'>
-                            MEIO AMBIENTE
-                        </Button>
-                    </div>
+                </CardHeader>
+                <CardBody className={styles.card}>
+                    <Row >
+                        <Col color="warning">
+                            <Card>
+                                <Button href='./semma/home' outline color="success">
+                                    <div className="m-3">
+                                        <h1>ENTRAR COMO</h1>
+                                    </div>
 
-                </Col>
-            </Row>
+                                    <div>
+                                        <img src="SEMMA_LOGO.png" className={styles.imagem} />
+                                    </div>
+                                    <h4>SEMMA</h4>
+                                </Button>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card>
+                                <Button href='./home' outline color="warning">
+                                    <div className="m-3">
+                                        <h1>ENTRAR COMO</h1>
+                                    </div>
+
+                                    <div>
+                                        <img src="DFCIVIL.png" className={styles.imagem} />
+                                    </div>
+                                    <h4>MEIO AMBIENTE</h4>
+                                </Button>
+                            </Card>
+
+                        </Col>
+                    </Row>
+                </CardBody>
+            </div>
         </div>
     )
 }
